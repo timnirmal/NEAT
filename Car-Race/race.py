f0fee683d66b385b24cae0d1fe6b1dda959dd45a
timnirmal <timnirmal@gmail.com>
2022-01-29 21:23:13 +0530

    Map Done

---
+++ Car-Race/race.py
@@ -0,0 +1,48 @@
+import pygame
+import math
+import sys
+import neat
+
+print(pygame.ver)
+
+screen_width = 1500
+screen_height = 800
+generation = 1
+
+
+def run_car():
+    # Init NEAT
+    nets = []
+    cars = []
+
+    # Init pygame
+    pygame.init()
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    clock = pygame.time.Clock()
+    map = pygame.image.load("map.png")
+
+    # Main Loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit(0)
+
+        # Draw map
+        screen.blit(map, (0, 0))
+
+        # Draw cars
+        for i, car in enumerate(cars):
+            car.draw(screen)
+            # Get output from NEAT
+            output = nets[i].activate((car.x, car.y, car.angle, car.speed))
+            # Update car
+            car.update(output[0], output[1])
+
+        # Update screen
+        pygame.display.flip()
+        clock.tick(60)
+
+
+if __name__ == '__main__':
+    run_car()
